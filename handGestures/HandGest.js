d1edb31bf50984065dcad35a1ee85d8043a792e6
jacoblovins <lovins.jacob@yahoo.com>
2020-11-11 11:58:43 -0500

    initail functionality

---
+++ handGestures/HandGest.js
@@ -0,0 +1,159 @@
+import React, { useRef, useEffect } from 'react'
+import * as Webcam from 'react-webcam';
+import * as ml5 from 'ml5';
+import './gesture.css'
+let brain;
+let inputs;
+let pose;
+
+const poseParameters = {
+    pose1: "Up",
+    pose2: "Down",
+    pose3: "Left",
+    pose4: "Right",
+    classifySpeed: 300,
+    webcamWidth: 640,
+    webcamHeight: 480,
+    videoHidden: false
+}
+
+
+
+function HandGest() {
+    let working = false;
+    const webcamRef = useRef(null);
+
+    const runHandpose = async () => {
+        // set up the video parameters to work with the model
+        const video = await webcamRef.current.video;
+        const videoWidth = video.videoWidth;
+        const videoHeight = video.videoHeight;
+        video.width = videoWidth;
+        video.height = videoHeight;
+        console.log(video.width);
+
+        // load the pre-made handpose model and pass the video into it
+        // and notify when its loaded
+        const handpose = ml5.handpose(video, modelLoaded);
+        function modelLoaded() {
+            console.log('Model Loaded!');
+        }
+
+        // anytime the handpose model sees a hand, run the detect funtion
+        handpose.on('predict', detect);
+
+        // set up my neural network parameters
+        let options = {
+            inputs: 42,
+            outputs: 4,
+            task: 'classification',
+            debug: true
+        }
+        brain = ml5.neuralNetwork(options);
+
+        // load the trained gesture data from the files and 
+        // notify when it's ready
+        const modelInfo = {
+            model: 'data/model.json',
+            metadata: 'data/model_meta.json',
+            weights: 'data/model.weights.bin'
+        }
+        brain.load(modelInfo, brainReady);
+        function brainReady() {
+            console.log('brain ready');
+        }
+    };
+
+    useEffect(() => {
+        runHandpose()
+    }, [])
+
+    // Start collecting the poses if there are any
+    function detect(poses) {
+        if (poses.length > 0) {
+            working = true
+            pose = poses[0];
+        } else {
+            working = false
+        }
+    };
+
+    // tell the app to start classifying the poses at the 
+    // speed determined by the 'classifySpeed' variable at the top
+    let inter;
+    function startClass() {
+        working = true
+        inter = setInterval(() => {
+            classifyPose()
+        }, poseParameters.classifySpeed);
+    }
+
+    // stop reading gestures
+    function stopClass() {
+        working = false
+        clearInterval(inter)
+    }
+
+    // send the incoming pose data to the model and
+    // classify the data from each pose into gestures
+    function classifyPose() {
+        if (pose && working) {
+            inputs = [];
+            for (let i = 0; i < pose.landmarks.length; i++) {
+                inputs.push(pose.landmarks[i][0]);
+                inputs.push(pose.landmarks[i][1]);
+            }
+            brain.classify(inputs, gotResult)
+        }
+    }
+
+    // do something with the gesture results
+    function gotResult(error, results) {
+        if (results[0].confidence > 0.75) {
+            const gesture = results[0].label
+            console.log(gesture)
+
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ADD LOGIC FOR WHAT YOU WANT EACH GESTURE TO DO HERE
+// -----------------------------------------------------------------------------------------------------------------------
+
+            if (gesture === poseParameters.pose1) {
+                window.scrollBy(0, -50);
+
+            } else if (gesture === poseParameters.pose2) {
+                window.scrollBy(0, 50);
+
+            } else if (gesture === poseParameters.pose3) {
+                // window.scrollBy(0, 50);
+
+            } else if (gesture === poseParameters.pose4) {
+                // window.scrollBy(0, 50);
+
+            }
+
+        }
+    }
+// -----------------------------------------------------------------------------------------------------------------------
+// 
+// -----------------------------------------------------------------------------------------------------------------------
+
+    return (
+        <div>
+            <Webcam ref={webcamRef}
+                style={{
+                    width: poseParameters.webcamWidth,
+                    height: poseParameters.webcamHeight,
+                }} />
+            <button onClick={() => startClass()}>Classify</button>
+            <button onClick={() => stopClass()}>Stop</button>
+        </div>
+    );
+}
+
+module.exports = {
+    HandGest,
+    poseParameters
+} 
+    
+
